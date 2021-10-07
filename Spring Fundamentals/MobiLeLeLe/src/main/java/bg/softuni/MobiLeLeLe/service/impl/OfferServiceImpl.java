9ae2e3c635394ec9463b6116d49caa3e831db1c4
Gavriel Dunev <dunev17@gmail.com>
2021-10-07 12:06:34 +0300

    Initialize offers.

--- Spring Fundamentals/MobiLeLeLe/src/main/java/bg/softuni/MobiLeLeLe/service/impl/OfferServiceImpl.java
+++ Spring Fundamentals/MobiLeLeLe/src/main/java/bg/softuni/MobiLeLeLe/service/impl/OfferServiceImpl.java
@@ -1,11 +1,17 @@
 package bg.softuni.MobiLeLeLe.service.impl;
 
 import bg.softuni.MobiLeLeLe.model.entity.OfferEntity;
+import bg.softuni.MobiLeLeLe.model.entity.enums.EngineEnum;
+import bg.softuni.MobiLeLeLe.model.entity.enums.TransmissionEnum;
+import bg.softuni.MobiLeLeLe.repository.ModelRepository;
 import bg.softuni.MobiLeLeLe.repository.OfferRepository;
+import bg.softuni.MobiLeLeLe.repository.UserRepository;
 import bg.softuni.MobiLeLeLe.service.OfferService;
 import bg.softuni.MobiLeLeLe.view.OfferSummaryView;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,14 +19,44 @@ import java.util.stream.Collectors;
 public class OfferServiceImpl implements OfferService {
 
     private final OfferRepository offerRepository;
+    private final ModelRepository modelRepository;
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-    public OfferServiceImpl(OfferRepository offerRepository) {
+    public OfferServiceImpl(OfferRepository offerRepository, ModelRepository modelRepository, UserRepository userRepository, ModelMapper modelMapper) {
         this.offerRepository = offerRepository;
+        this.modelRepository = modelRepository;
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
     }
 
     @Override
     public void initializeOffers() {
-        //TODO:
+        if (this.offerRepository.count() == 0) {
+            OfferEntity firstOffer = new OfferEntity()
+                    .setDescription("New")
+                    .setEngine(EngineEnum.GASOLINE)
+                    .setImageUrl("https://dizzyriders.bg/uploads/thumbs/gallery/2021-04/408987c4b325668c030d415b85c1a319-620x427.jpg")
+                    .setMileage(20)
+                    .setModel(this.modelRepository.findById(1L).orElse(null))
+                    .setPrice(BigDecimal.valueOf(200000))
+                    .setSeller(this.userRepository.findByUsername("gosho").orElse(null))
+                    .setTransmission(TransmissionEnum.AUTOMATIC)
+                    .setYear(2021);
+
+            OfferEntity secondOffer = new OfferEntity()
+                    .setDescription("Very well serviced and in good condition.")
+                    .setEngine(EngineEnum.DIESEL)
+                    .setImageUrl("https://i.ytimg.com/vi/GlKYm70zyu4/maxresdefault.jpg")
+                    .setMileage(100000)
+                    .setModel(this.modelRepository.findById(2L).orElse(null))
+                    .setPrice(BigDecimal.valueOf(40000))
+                    .setSeller(this.userRepository.findByUsername("admin").orElse(null))
+                    .setTransmission(TransmissionEnum.MANUAL)
+                    .setYear(2015);
+
+            this.offerRepository.saveAll(List.of(firstOffer, secondOffer));
+        }
     }
 
     @Override
@@ -30,7 +66,10 @@ public class OfferServiceImpl implements OfferService {
     }
 
     private OfferSummaryView map(OfferEntity offerEntity) {
-        //TODO:
-        return new OfferSummaryView();
+        OfferSummaryView offerSummaryView = modelMapper.map(offerEntity, OfferSummaryView.class);
+
+        offerSummaryView.setModel(offerEntity.getModel().getName());
+
+        return offerSummaryView;
     }
 }
