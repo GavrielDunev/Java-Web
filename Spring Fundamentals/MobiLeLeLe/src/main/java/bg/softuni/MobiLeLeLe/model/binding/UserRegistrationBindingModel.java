bf2aabeba85c8c108be86836089005f7e484c999
Gavriel Dunev <dunev17@gmail.com>
2021-10-15 13:03:27 +0300

    Unique username custom annotation added.

--- Spring Fundamentals/MobiLeLeLe/src/main/java/bg/softuni/MobiLeLeLe/model/binding/UserRegistrationBindingModel.java
+++ Spring Fundamentals/MobiLeLeLe/src/main/java/bg/softuni/MobiLeLeLe/model/binding/UserRegistrationBindingModel.java
@@ -1,5 +1,7 @@
 package bg.softuni.MobiLeLeLe.model.binding;
 
+import bg.softuni.MobiLeLeLe.model.validator.UniqueUsername;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -13,6 +15,7 @@ public class UserRegistrationBindingModel {
     private String lastName;
     private String password;
     private String confirmPassword;
+    @UniqueUsername
     private String username;
 
 
