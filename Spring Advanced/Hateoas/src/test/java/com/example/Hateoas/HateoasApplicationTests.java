58def45e0f23f1e41d009387c74f24b74d51eccb
Gavriel Dunev <dunev17@gmail.com>
2021-11-03 15:07:53 +0200

    Added entities.

---
+++ Spring Advanced/Hateoas/src/test/java/com/example/Hateoas/HateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
